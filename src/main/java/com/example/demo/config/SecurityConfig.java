b5688c6a7f57924f78b8ae4970a17a0531a66b98
BaeJooHwan <kyoung3774@naver.com>
2024-11-14 17:25:39 +0900

    A

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -16,9 +16,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-		// authorizeHttpRequests(): 특정 경로에 대한 접근 권한 부여
-        .authorizeHttpRequests((auth) -> auth 
+			
+		http.authorizeHttpRequests((auth) -> auth // authorizeHttpRequests(): 특정 경로에 대한 접근 권한 부여
         		// requestMatchers(): 경로 설정 / 주의사항: 동작순서 위~아래
                 .requestMatchers("/", "/login").permitAll() // permitAll(): 접근 권한 부여(모든 유저 가능)
                 .requestMatchers("/admin").hasRole("ADMIN") // hasRole(): 접근 권한 부여 단건(인자값 권한만 가능)
@@ -27,10 +26,17 @@ public class SecurityConfig {
                 .anyRequest().authenticated() // authenticated(): 접근 권한 부여(로그인한 유저만 가능)
                 
         );
-			// formLogin(): 권한(로그인)이 필요한 API(페이지)를 요청 했을때 권한이 없을 시 엑세스 거부 페이지가 아닌 로그인 경로 페이지로 유도해주는 메소드
-		http.formLogin((auth) -> auth
-				.loginPage("/login") // ㅣloginPage(): 커스텀한 로그인 페이지 경로를 인수값으로 설정
-				.loginProcessingUrl(null))
+			
+		http.formLogin((auth) -> auth // formLogin(): 권한(로그인)이 필요한 API(페이지)를 요청 했을때 권한이 없을 시 엑세스 거부 페이지가 아닌 로그인 경로 페이지로 유도해주는 메소드
+				.loginPage("/login") // loginPage(): 커스텀한 로그인 페이지 유도할 경로를 인수값으로 설정
+				.loginProcessingUrl("/loginProc") // loginProcessingUrl(): 커스텀 로그인 <from>에 작성한 데이터를 스프링 시큐리티가 로그인 처리
+				.permitAll() // permitAll(): 접근 권한 부여(모든 유저 가능)
+		);
+		
+		// csrf(): 사이트 위조 방지를 위한 함수
+		// 로그인 할 때 커스텀 로그인 <from>에 작성한 데이터뿐만이 아닌 csrf토큰이 필요함
+		http.csrf((auth) -> auth.disable()); // disable(): csrf토큰의 필요를 없애는 메소드
+		
 		return http.build();
 	}
 }
